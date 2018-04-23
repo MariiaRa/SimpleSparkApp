021983b402724a17a7f4b6a5091f970d04698c5f
Maria Rabyk <mariia.rabyk@gmail.com>
2018-04-23 16:15:38 +0300

    Edit file

--- src/main/scala/com/ua/MainDS.scala
+++ src/main/scala/com/ua/MainDS.scala
@@ -9,7 +9,7 @@ object MainDS {
   def main(args: Array[String]): Unit = {
 
     val category = args(0)
-    val year = args(1)
+    val year = args(1).toInt
     val country = args(2)
     val file = args(3)
 
@@ -47,42 +47,38 @@ object MainDS {
     // val dataSet = DFCsv.as[CropsData]
 
     val DS2 = noNullDSCsv.map(row => CropsData(row.getAs[String](0).trim, row.getAs[Integer](1), row.getAs[String](2),
-      row.getAs[Integer](3), row.getAs[String](4), row.getAs[Long](5), row.getAs[String](6), row.getAs[String](7)))
-    //  DS2.show()
-
-    //select columns of interest
-    val selected = DS2.select($"country_or_area".alias("Country"), $"year".alias("Year"), $"element".alias("Element"),
-      $"value".alias("Value"), $"category".alias("Category"))
+                              row.getAs[Integer](3), row.getAs[String](4), row.getAs[Long](5), row.getAs[String](6),
+                              row.getAs[String](7)))
 
     //filter out regions
-    val countries = selected.where(
-      !$"Country".contains("World") &&
-        !selected.col("Country").contains("Asia") &&
-        !selected.col("Country").contains("Africa") &&
-        !selected.col("Country").contains("America") &&
-        !selected.col("Country").contains("Americas") &&
-        !selected.col("Country").contains("Europe") &&
-        !selected.col("Country").contains("European Union") &&
-        !selected.col("Country").contains("Australia and New Zealand") &&
-        !selected.col("Country").like("%Countries") &&
-        !selected.col("Country").like("%countries") &&
-        !selected.col("Country").contains("Small Island Developing States")
+    val countries = DS2.filter(crop => !crop.country_or_area.equals("World")
+      && !crop.country_or_area.contains("Asia") && !crop.country_or_area.contains("Africa")
+      && !crop.country_or_area.equals("Asia") && !crop.country_or_area.contains("America")
+      && !crop.country_or_area.contains("Americas") && !crop.country_or_area.contains("Europe")
+      && !crop.country_or_area.equals("European Union") && !crop.country_or_area.equals("Australia and New Zealand")
+      && !crop.country_or_area.equals("Small Island Developing States") && !crop.country_or_area.contains("Countries")
+      && !crop.country_or_area.contains("countries") && !crop.country_or_area.equals("Low Income Food Deficit Countries")
     )
 
     //total production
-    val totalProduction = countries.where($"Category" === category && $"Year" === year && $"Element" === "Production Quantity")
-      .agg(sum("Value").alias("Total_Production"))
+    val totalProduction = countries.filter(crop => crop.element == "Production Quantity" && crop.category.equals(category) &&
+      crop.year == year).agg(sum("value").alias("Total_Production"))
 
     //  min, max, avg
-    val minMaxAvg = selected.where($"category" === category && $"Country" === country && $"Element" === "Production Quantity")
+    val minMaxAvg = countries.filter(crop => crop.element == "Production Quantity"
+      && crop.category.equals(category)
+      && crop.country_or_area == country)
       .agg(min("value").alias("Min Production"), max("value").alias("Max Production"), avg("value").alias("Average Production"))
 
-
     //top productive year in world
-    val topProducers = countries.where($"category" === category && $"year" === year && $"Element" === "Production Quantity").sort(desc("value"))
+    val topProducers = countries.filter(crop => crop.element == "Production Quantity"
+      && crop.category.equals(category)
+      && crop.year == year).sort(desc("value"))
 
     //top productive year by country
-    val topYear = countries.where($"category" === category && $"Country" === country && $"Element" === "Production Quantity").sort(desc("value"))
+    val topYear = countries.filter(crop => crop.element == "Production Quantity"
+      && crop.category.equals(category)
+      && crop.country_or_area == country).sort(desc("value"))
 
     minMaxAvg.write
       .format("csv")
