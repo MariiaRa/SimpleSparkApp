246895f2c11e48fa68e79352226e1fcb7019f22b
Maria Rabyk <mariia.rabyk@gmail.com>
2018-04-26 18:09:50 +0300

    Rename class

---
+++ src/main/scala/com/ua/Entity/ProductData.scala
@@ -0,0 +1,5 @@
+package com.ua.Entity
+
+case class ProductData(country_or_area: String, element_code: Int, element: String, year: Int, unit: String, value: Long, value_footnotes: String,
+                       category: String)
+
