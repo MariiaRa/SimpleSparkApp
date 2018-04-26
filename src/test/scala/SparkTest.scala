d84556f8145463d60621c92c883f67f71fe14ff6
Maria Rabyk <mariia.rabyk@gmail.com>
2018-04-26 18:44:52 +0300

    Edit file

--- src/test/scala/SparkTest.scala
+++ src/test/scala/SparkTest.scala
@@ -1,45 +1,44 @@
-import com.ua.Entity.{CropStats, CropsData}
+import com.ua.Entity.{ProductStats, ProductData}
 import org.apache.spark.sql.{Dataset, SparkSession}
 import org.scalatest.FunSuite
 
 class SparkTest extends FunSuite {
-  val sparkSession = SparkSession.builder.
-    master("local")
+  val sparkSession = SparkSession.builder
     .appName("myApp")
     .getOrCreate()
 
   import sparkSession.implicits._
 
-  val statistics = new CropStats
+  val statistics = new ProductStats
 
   val list = List(
-    CropsData("Afghanistan", 51, "Production Quantity", 1994, "tonnes", 17500, "F", "apples"),
-    CropsData("Afghanistan", 31, "Area Harvested", 2007, "Ha", 2350, "F", "apples"),
-    CropsData("Zimbabwe", 51, "Production Quantity", 1980, "tonnes", 0, "NR", "anise_badian_fennel_corian"),
-    CropsData("Yugoslav SFR", 51, "Production Quantity", 1965, "tonnes", 41500, "blanck", "cherries"),
-    CropsData("Algeria", 51, "Production Quantity", 2001, "tonnes", 104900, "blanck", "apples"),
-    CropsData("Algeria", 51, "Production Quantity", 1972, "tonnes", 11480, "blanck", "apples"),
-    CropsData("Armenia", 51, "Production Quantity", 2001, "tonnes", 35380, "blanck", "apples"),
-    CropsData("Australia", 51, "Production Quantity", 1964, "tonnes", 367397, "blanck", "apples"),
-    CropsData("Albania", 51, "Production Quantity", 1965, "tonnes", 0, "NR", "other_melons_inc_cantaloupes")
+    ProductData("Afghanistan", 51, "Production Quantity", 1994, "tonnes", 17500, "F", "apples"),
+    ProductData("Afghanistan", 31, "Area Harvested", 2007, "Ha", 2350, "F", "apples"),
+    ProductData("Zimbabwe", 51, "Production Quantity", 1980, "tonnes", 0, "NR", "anise_badian_fennel_corian"),
+    ProductData("Yugoslav SFR", 51, "Production Quantity", 1965, "tonnes", 41500, "blanck", "cherries"),
+    ProductData("Algeria", 51, "Production Quantity", 2001, "tonnes", 104900, "blanck", "apples"),
+    ProductData("Algeria", 51, "Production Quantity", 1972, "tonnes", 11480, "blanck", "apples"),
+    ProductData("Armenia", 51, "Production Quantity", 2001, "tonnes", 35380, "blanck", "apples"),
+    ProductData("Australia", 51, "Production Quantity", 1964, "tonnes", 367397, "blanck", "apples"),
+    ProductData("Albania", 51, "Production Quantity", 1965, "tonnes", 0, "NR", "other_melons_inc_cantaloupes")
   )
 
   test("total production") {
-    val input: Dataset[CropsData] = sparkSession.createDataset[CropsData](list)
+    val input: Dataset[ProductData] = sparkSession.createDataset[ProductData](list)
     val result = statistics.getTotalProduction(input, "apples", 2001).head().getAs[Long](0)
     val expected = 140280
     assert(expected == result)
   }
 
   test("top productive year") {
-    val input: Dataset[CropsData] = sparkSession.createDataset[CropsData](list)
+    val input: Dataset[ProductData] = sparkSession.createDataset[ProductData](list)
     val result = statistics.getTopYear(input, "apples", "Algeria").head().year
     val expected = 2001
     assert(expected == result)
   }
 
   test("compare top producers") {
-    val input: Dataset[CropsData] = sparkSession.createDataset[CropsData](list)
+    val input: Dataset[ProductData] = sparkSession.createDataset[ProductData](list)
     val result = statistics.getTopProducers(input, "apples", 2001).select("country_or_area").take(10).map(row => row.getString(0)).toList
     val expected = List("Algeria", "Armenia")
     assert(expected == result)
