14d14d551b556a8344c5cb329ef6a4152274daa9
Maria Rabyk <mariia.rabyk@gmail.com>
2018-04-26 18:45:28 +0300

    Delete files

--- src/main/scala/com/ua/Entity/CropsData.scala
+++
@@ -1,5 +0,0 @@
-package com.ua.Entity
-
-case class CropsData (country_or_area: String, element_code: Int, element: String, year: Int, unit: String, value: Long, value_footnotes: String,
-                     category: String)
-
